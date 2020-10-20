4525f2140240b1ea65fd1349a9b77b6f835ba677
Dscoe <zdwzhaoyikai3@gmail.com>
2020-10-20 10:54:17 -0500

    added tab_ae

--- Yikai_helper_funcs/_nbdev.py
+++ Yikai_helper_funcs/_nbdev.py
@@ -2,9 +2,14 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"ReadTabBatchIdentity": "tab_ae.ipynb",
+         "TabularPandasIdentity": "tab_ae.ipynb",
+         "TabDataLoaderIdentity": "tab_ae.ipynb",
+         "RecreatedLoss": "tab_ae.ipynb",
+         "BatchSwapNoise": "tab_ae.ipynb",
+         "TabularAE": "tab_ae.ipynb"}
 
-modules = []
+modules = ["tab_ae.py"]
 
 doc_url = "https://DavidykZhao.github.io/Yikai_helper_funcs/"
 
