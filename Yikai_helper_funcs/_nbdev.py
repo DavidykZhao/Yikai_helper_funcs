150e418306032ae55a3213f94e59a5f1bb79025d
Dscoe <zdwzhaoyikai3@gmail.com>
2020-10-20 11:02:16 -0500

    updated index

--- Yikai_helper_funcs/_nbdev.py
+++ Yikai_helper_funcs/_nbdev.py
@@ -2,14 +2,16 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"ReadTabBatchIdentity": "tab_ae.ipynb",
+index = {"say_hello": "00_core.ipynb",
+         "ReadTabBatchIdentity": "tab_ae.ipynb",
          "TabularPandasIdentity": "tab_ae.ipynb",
          "TabDataLoaderIdentity": "tab_ae.ipynb",
          "RecreatedLoss": "tab_ae.ipynb",
          "BatchSwapNoise": "tab_ae.ipynb",
          "TabularAE": "tab_ae.ipynb"}
 
-modules = ["tab_ae.py"]
+modules = ["core.py",
+           "tab_ae.py"]
 
 doc_url = "https://DavidykZhao.github.io/Yikai_helper_funcs/"
 
